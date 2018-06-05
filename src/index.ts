98ee4b14c799d195b8b82c010390ce94d10dc3d6
Alex M <me@itsalex.fr>
2018-06-05 23:46:31 +0200

    WIP: search on channels

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,38 @@
+const fs = require('fs')
+const map = require('async/map')
 const { WebClient } = require('@slack/client')
 
-const token = process.env.SAAR_SLACK_TOKEN
+const web = new WebClient(process.env.SAAR_SLACK_TOKEN)
 
-const web = new WebClient(token)
+let main = async () => {
+  let users: any = {}
 
-interface SlackWeb {
-  channels: string;
+  if (typeof process.env.SAAR_SLACK_CHANNEL === 'undefined') {
+    console.log('No channel given')
+    return
+  }
+
+  try {
+    users = await web.apiCall('conversations.members', { channel: process.env.SAAR_SLACK_CHANNEL })
+  } catch (err) {
+    console.log(err)
+  }
+
+  await map(users.members, async (user: any, cb: Function) => {
+    try {
+      let res: any = await web.apiCall('users.info', { user: user })
+      cb(null, res)
+    } catch (err) {
+      cb(err)
+    }
+  }, (err: Error, results: any) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+
+    fs.writeFileSync('./users.json', JSON.stringify(results))
+  })
 }
 
-web.channels.list()
-  .then((res: SlackWeb) => {
-    // `res` contains information about the channels
-    res.channels.forEach(c => console.log(c.name));
-  })
-  .catch(console.error);
-});
+main()
